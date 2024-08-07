ee4be47d0b379e7d436f7c65a080a224fe73ede4
benjaminvasquez07 <benjaminvasquez289@gmail.com>
2024-08-07 11:51:19 -0300

    testie la funcionalidad de usuaria resgistrado y email resgristrado

--- examples/tutorial/flaskr/auth.py
+++ examples/tutorial/flaskr/auth.py
@@ -53,6 +53,7 @@ def register():
     if request.method == "POST":
         username = request.form["username"]
         password = request.form["password"]
+        email = request.form["email"]
         db = get_db()
         error = None
 
@@ -60,18 +61,26 @@ def register():
             error = "Usuario incorrecto"
         elif not password:
             error = "Contraseña incorrecta"
+        elif not email:
+            error = "Contraseña incorrecta"
 
         if error is None:
             try:
                 db.execute(
-                    "INSERT INTO user (username, password) VALUES (?, ?)",
-                    (username, generate_password_hash(password)),
+                    "INSERT INTO user (username, password,email) VALUES (?, ?, ?)",
+                    (username, generate_password_hash(password),email),
                 )
                 db.commit()
-            except db.IntegrityError:
-                # The username was already taken, which caused the
-                # commit to fail. Show a validation error.
-                error = f"Usuario {username} ya esta registrado."
+            except db.IntegrityError as e:
+                mensaje = e.args[0]
+                print(mensaje)
+                if "user.username" in mensaje:
+                    error = f"Usuario {username} esta registrado"
+                elif "user.email" in mensaje:
+                    error = f"mail {email} ya esta registrado"
+                else:
+                    error = "error desconocido"
+
             else:
                 # Success, go to the login page.
                 return redirect(url_for("auth.login"))
--- examples/tutorial/tests/test_auth.py
+++ examples/tutorial/tests/test_auth.py
@@ -10,7 +10,7 @@ def test_register(client, app):
     assert client.get("/auth/register").status_code == 200
 
     # test that successful registration redirects to the login page
-    response = client.post("/auth/register", data={"username": "a", "password": "a"})
+    response = client.post("/auth/register", data={"username": "a", "password": "a", "email":"b"})
     assert response.headers["Location"] == "/auth/login"
 
     # test that the user was inserted into the database
@@ -22,16 +22,17 @@ def test_register(client, app):
 
 
 @pytest.mark.parametrize(
-    ("username", "password", "message"),
+    ("username", "password","email", "message"),
     (
-        ("", "", "Usuario incorrecto"),
-        ("a", "", "Contraseña incorrecta"),
-        ("test", "test", "ya esta registrado."),
+        ("", "a","a", "Usuario incorrecto"),
+        ("a", "","a" ,"Contraseña incorrecta"),
+        ("test", "test","test", "esta registrado"),
+        ("a", "a","b@gmail", "ya esta registrado"),
     ),
 )
-def test_register_validate_input(client, username, password, message):
+def test_register_validate_input(client, username, password,email, message):
     response = client.post(
-        "/auth/register", data={"username": username, "password": password}
+        "/auth/register", data={"username": username, "password": password, "email":email}
     )
     assert message in response.data.decode()
 
